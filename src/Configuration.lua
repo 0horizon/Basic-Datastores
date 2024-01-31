f627f948e49566349599c62c2bd371effd2368fe
TotalHorizons <158045741+0horizon@users.noreply.github.com>
2024-01-31 16:26:42 +0200

    Create Configuration.lua

---
+++ src/Configuration.lua
@@ -0,0 +1,11 @@
+-- // Credits \\
+-- TotalHorizons / @0horizon
+-- MIT License
+
+return {
+	GeoBlock = false,
+	BannedCountries = {"US"},
+	
+	GroupBans = false,
+	BannedGroups = {15325, 35135, 523563}
+}
